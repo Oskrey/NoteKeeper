dee3e7108f4c4c62bcc4258cbcb56eb5028a78bb
Oskrey <pedi hn@gmail. com>
2023-03-11 04:43:04 +0300

    monke 🍌🐒

--- app/src/main/java/com/example/notes/ui/NotesAdapter.java
+++ app/src/main/java/com/example/notes/ui/NotesAdapter.java
@@ -17,7 +17,7 @@ import java.util.Locale;
 /**
  * Адаптер для заметок
  */
-public class NotesAdapter extends CursorRecyclerAdapter<NotesAdapter.ViewHolder> {
+    public class NotesAdapter extends CursorRecyclerAdapter<NotesAdapter.ViewHolder> {
 
     private final OnNoteClickListener onNoteClickListener;
     private int catID;
@@ -25,16 +25,14 @@ public class NotesAdapter extends CursorRecyclerAdapter<NotesAdapter.ViewHolder>
         super(cursor);
         this.catID = catID;
 
-
         this.onNoteClickListener = onNoteClickListener;
-        return;
 
     }
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, Cursor cursor) {
 
-        //if(catID == -1){
+
             int idColumnIndex = cursor.getColumnIndexOrThrow(NotesContract.Notes._ID);
             long id = cursor.getLong(idColumnIndex);
 
@@ -50,46 +48,16 @@ public class NotesAdapter extends CursorRecyclerAdapter<NotesAdapter.ViewHolder>
             Date date = new Date(updatedTs);
 
             viewHolder.dateTv.setText(viewHolder.SDF.format(date));
-        /*}
-        else {//кринжатина
-            int columnIndex = cursor.getColumnIndexOrThrow(NotesContract.Notes.COLUMN_CATEGORY);
-            int cat = cursor.getInt(columnIndex);
 
-            if (cat == catID){
-                int idColumnIndex = cursor.getColumnIndexOrThrow(NotesContract.Notes._ID);
-                long id = cursor.getLong(idColumnIndex);
-
-                viewHolder.itemView.setTag(id);
-
-                int titleColumnIndex = cursor.getColumnIndexOrThrow(NotesContract.Notes.COLUMN_TITLE);
-                String title = cursor.getString(titleColumnIndex);
-
-                viewHolder.titleTv.setText(title);
-
-                int dateColumnIndex = cursor.getColumnIndexOrThrow(NotesContract.Notes.COLUMN_UPDATED_TS);
-                long updatedTs = cursor.getLong(dateColumnIndex);
-                Date date = new Date(updatedTs);
-
-                viewHolder.dateTv.setText(viewHolder.SDF.format(date));
-            }
-            else return;
-        }*/
 
     }
     //@Nullable
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        //int columnIndex = cursor.getColumnIndexOrThrow(NotesContract.Notes.COLUMN_CATEGORY);
-        //int cat = cursor.getInt(columnIndex);
 
-        //if (cat == catID){
             LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-
             View view = layoutInflater.inflate(R.layout.view_item_note, parent, false);
-
             return new ViewHolder(view);
-        // } else return null;
-
     }
 
     /**
@@ -106,8 +74,6 @@ public class NotesAdapter extends CursorRecyclerAdapter<NotesAdapter.ViewHolder>
             super(itemView);
             this.titleTv = itemView.findViewById(R.id.title_tv);
             this.dateTv = itemView.findViewById(R.id.date_tv);
-            //if (catID == -1){
-
 
                 itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -117,25 +83,6 @@ public class NotesAdapter extends CursorRecyclerAdapter<NotesAdapter.ViewHolder>
                         onNoteClickListener.onNoteClick(noteId);
                     }
                 });
-           /* }else {//кринжатина
-                int columnIndex = cursor.getColumnIndexOrThrow(NotesContract.Notes.COLUMN_CATEGORY);
-                int cat = cursor.getInt(columnIndex);
-
-                if (cat == catID){
-
-
-                    itemView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            long noteId = (Long) v.getTag();
-
-                            onNoteClickListener.onNoteClick(noteId);
-                        }
-                    });
-
-                } else return;
-            }*/
-
         }
 
     }
